37dd2f26a99ef167fb0ef4a69a55ec606bd3852b
connoryim <0134269333+connoryim@users.noreply.github.com>
2023-12-11 13:59:21 -0500

    Make a functionto reset grid

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const grid = document.querySelector("#grid");
 const div = document.createElement("div");
 
-for(let i=0; i<16; i++){
-    const row = document.createElement("div");
-    row.classList.add("row");
-    for(let j=0; j<16; j++){
-        const column = document.createElement("div");
-        column.classList.add("column");
-        row.appendChild(column);
-    }
-    grid.appendChild(row);
+
+function newGrid() {
+    for(let i=0; i<16; i++){
+        const row = document.createElement("div");
+        row.classList.add("row");
+        for(let j=0; j<16; j++){
+            const column = document.createElement("div");
+            column.classList.add("column");
+            row.appendChild(column);
+        }
+        grid.appendChild(row);
+    };
 };
+
+const reset = document.querySelector("#reset");
+reset.addEventListener("click",(e)=>{
+    var rows = document.getElementsByClassName("row");
+    while (rows[0]) {
+        rows[0].parentNode.removeChild(rows[0]);
+    }
+    newGrid();
+})
+
+newGrid();
