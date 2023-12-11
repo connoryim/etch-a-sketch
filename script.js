ce808ca2eabb6eb54888e2c1018c476cf1df6b06
connoryim <0134269333+connoryim@users.noreply.github.com>
2023-12-11 04:28:16 -0500

    Make grid visible using padding

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const div = document.createElement("div");
 
 for(let i=0; i<16; i++){
     const row = document.createElement("div");
+    row.classList.add("row");
     for(let j=0; j<16; j++){
         const column = document.createElement("div");
+        column.classList.add("column");
         row.appendChild(column);
     }
     grid.appendChild(row);
